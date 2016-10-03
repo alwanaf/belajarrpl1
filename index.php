dd1dd32cdac7ec2051ae8f7636ff6b00e9e3d7d3
Maulana Amsor Sidik <maulanaamsor9@gmail.com>
2016-10-03 16:24:30 +0700

    Commit tambah nim dan nama

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 echo("10114137 Alwan Ammar Fauzan");
 echo("10114134 Arifano Teguh Wicaksono");
-echo("10114135 Aditya Nugraha")
+echo("10114135 Aditya Nugraha");
+echo("10114105 Maulana Amsor Sidik");
 ?>
