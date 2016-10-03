050b7b3f9f99ff9168d075a5a156d72ae50517db
aditya10n <aditya10nugraha@gmail.com>
2016-10-03 11:18:56 +0700

    rapihin gan

--- index.php
+++ index.php
@@ -1,18 +1,5 @@
 <?php
-<<<<<<< HEAD
-<<<<<<< HEAD
 echo("10114137 Alwan Ammar Fauzan");
 echo("10114134 Arifano Teguh Wicaksono");
-=======
-echo("10114137 Alwan Ammar Fauzan")
 echo("10114135 Aditya Nugraha")
->>>>>>> Test
-=======
-echo("10114137 Alwan Ammar Fauzan")
->>>>>>> f8fe1fa28623638fd3b813c73cc4ad84a044abd9
-
-echo("10114135 Aditya Nugraha")
-
-echo("10114135 Aditya Nugraha")
-echo("=======================");
 ?>
